38b9f85aa3b58cbaa2c006f097cc5b9a79ca9346
BeiChen <8983170+BeiChenGeGe@user.noreply.gitee.com>
2021-09-25 08:23:03 +0000

    删除文件 shixunblog/ceshi.py

--- shixunblog/ceshi.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python
-# -*- coding:utf-8 -*-
-# @author  : afeng
-# @time    : 2021/9/25 16:19
-# @function: the script is used to do something.
-# @version :
-a = 1
-b = 2
-print(a + b)
